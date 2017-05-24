b66120a4897a6aa6cd5b4058ab359d6cd7cc452a
Lucas <lucas@giter.com.br>
2017-05-24 02:41:35 -0300

    Insert method signature changed

--- src/dao/IDAO.java
+++ src/dao/IDAO.java
@@ -6,7 +6,7 @@ import model.Entity;
 
 public interface IDAO<T> {
 	public T select(Entity entDominio) throws SQLException;
-	public void insert(Entity entDominio) throws SQLException;
+	public Entity insert(Entity entDominio) throws SQLException;
 	public void update(Entity entDominio) throws SQLException;
 	public void delete(Entity entDominio) throws SQLException;
 }
